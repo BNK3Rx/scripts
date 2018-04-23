e98405c413f49b126ba838b5e908290b1965b5a6
P0L4K <35306347+Trihash@users.noreply.github.com>
2018-04-23 09:19:07 +0200

    Create Drupal-Detector-2.py

---
+++ Drupal/Drupal-Detector-2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env
+import sys
+import requests
+from multiprocessing.dummy import Pool
+
+requests.urllib3.disable_warnings()
+
+try:
+    target = [i.strip() for i in open(sys.argv[1], mode='r').readlines()]
+except IndexError:
+    exit('noob: pd.py list.txt')
+
+headers = {'User-Agent': 'Mozilla 5.0'}
+
+def run(u):
+    try:
+        url = u + '/misc/drupal.js' 
+        r = requests.post(url, verify=False, headers=headers)
+        if 'Drupal' in requests.get(u+'/misc/drupal.js', verify=False, headers=headers).text:
+            print ('\n\aDrupal:', u + '/misc/drupal.js\n')
+            with open('drudru2.txt', mode='a') as d:
+                d.write(u + '/\n')   
+        else:
+            print(u, " -> Not drupal")
+    except:
+        pass
+
+mp = Pool(120)
+mp.map(run, target)
+mp.close()
+mp.join()
